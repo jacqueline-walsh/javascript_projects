83f388790956ca855f7a138904e42663a90d994e
Jacqueline Walsh <jacquelinewalsh@jacquelines-mbp.home>
2020-03-07 19:34:43 +0000

    DOM Array Methods

---
+++ DOM_array_methods/script.js
@@ -0,0 +1,92 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// FUNCTIONS
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    addData(newUser);
+}
+
+// Double everyones money
+function doubleMoney() {
+    data = data.map(user => {
+        return {...user, money: user.money * 2};
+    });
+
+    updateDOM();
+}
+
+//  Sort all users by richest first
+function sortByRichest() {
+    data.sort((a, b) => (b.money - a.money));
+
+    updateDOM();
+}
+
+// filter list to show only Millionairs
+function showMillionairs() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+// Calculate the entire wealth of users
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
+// add new object to the data array
+function addData(obj) {
+    data.push(obj);
+
+    updateDOM();
+}
+
+// update DOM with user
+function updateDOM(provideData = data) {
+    // clear main div
+    main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+    provideData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// format number to money
+function formatMoney(number) {
+    return '€' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+// EVENT LISTENERS
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionairs);
+calculateWealthBtn.addEventListener('click', calculateWealth);
